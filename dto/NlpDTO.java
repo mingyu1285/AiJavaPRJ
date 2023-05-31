a2b8063845b72ef4e80ae0e31103d10e14c9d38f
mingyu1285 <56614072+mingyu1285@users.noreply.github.com>
2023-05-31 11:28:26 +0900

    Add files via upload

---
+++ dto/NlpDTO.java
@@ -0,0 +1,58 @@
+package kopo.poly.dto;
+
+import kopo.poly.service.INlpService;
+import kr.co.shineware.nlp.komoran.constant.DEFAULT_MODEL;
+import kr.co.shineware.nlp.komoran.core.Komoran;
+import kr.co.shineware.nlp.komoran.model.KomoranResult;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Getter
+@Setter
+@Slf4j
+@Service
+public class NlpDTO implements INlpService {
+
+    Komoran komoran = new Komoran(DEFAULT_MODEL.FULL); //학습용 데이터는 가장 큰 파일 사용
+
+    private String text; // 분석할 문장
+    private String result; // 분석된 품사 결과
+    private List<String> nouns; // 분석된 명사 추출 결과
+
+    @Override
+    public NlpDTO getPlainText(String text) {
+
+        log.info(this.getClass().getName()+ ".getPlainText Start!");
+
+        KomoranResult komoranResult = komoran.analyze(text); //인식된 문자열 결과
+
+        String result = komoranResult.getPlainText(); //모든 단어마다 품사 태깅
+
+        NlpDTO rDTO = new NlpDTO();
+        rDTO.setResult(result);
+
+        log.info(this.getClass().getName()+".getPlainText End!");
+
+        return rDTO;
+    }
+
+    @Override
+    public NlpDTO getNouns(String text) {
+
+        log.info(this.getClass().getName()+".getNouns Start!");
+
+        KomoranResult komoranResult = komoran.analyze(text); //인식된 문자열 분석 결과
+
+        List<String> nouns = komoranResult.getNouns(); //NNG, NNP 품사만 추출
+
+        NlpDTO rDTO = new NlpDTO();
+        rDTO.setNouns(nouns);
+
+        log.info(this.getClass().getName()+".getNouns End!");
+
+        return rDTO;
+    }
+}
